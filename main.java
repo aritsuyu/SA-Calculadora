fa11ee3af10957490f5f94c11a3347afe83b392e
aritsuyu <140478574+aritsuyu@users.noreply.github.com>
2025-06-06 09:40:27 -0300

    Alteração do nome do arquivo para Main

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import Lib.index;
-
-public class code {
+public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         index.menu(scanner);
